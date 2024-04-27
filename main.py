9ae82734f69f8b6e37e736210e14290ca3e68091
PedroCanoas <pedro28canoas@gmail.com>
2024-04-27 11:25:51 -0300

    projeto inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import ultralytics
+from ultralytics import YOLO
+
+ultralytics.checks()
+dir = 'datasets\\'
+
+# model = YOLO('yolov8n.yaml')  # build a new model from scratch
+model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
+
+results = model.train(data="config.yaml", epochs=50) # train the model
+results = model.val() # evaluate model performance on the validation set
+results = model([dir+'test\\images\\AUTOMOTIVE OIL\\1.jpg', dir+'test\\images\\AUTOMOTIVE OIL\\2.jpg', dir+'test\\images\\AUTOMOTIVE OIL\\3.jpg'])  # predict on an image
+
+# Process results list
+for i, result in enumerate(results):
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    result.show()  # display to screen
+    
+    # Construa o caminho completo para o arquivo
+    save_path = os.path.join(save_dir+'results', f'result_{i}.jpg')
+    
+    # Salve a imagem no caminho especificado
+    result.save(filename=save_path)
