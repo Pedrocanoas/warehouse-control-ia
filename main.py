2cbd93968cc96b1787445e6aeaf47a3b5c908097
PedroCanoas <pedro28canoas@gmail.com>
2024-08-14 18:20:49 -0300

    ajustes finais do modelo e ajustes nas imagens

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ dir = 'datasets\\'
 # model = YOLO('yolov8n.yaml')  # build a new model from scratch
 model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
 
-results = model.train(data="config.yaml", epochs=150) # train the model
-results = model.val() # evaluate model performance on the validation set
+results = model.train(data="config.yaml", epochs=200, patience=100, val=True) # train the model
+results = model.val(conf=0.001,save_json=True) # evaluate model performance on the validation set
 results = model([
     dir+'test\\images\\AUTOMOTIVE OIL\\1.jpg', 
     dir+'test\\images\\AUTOMOTIVE OIL\\2.jpg', 
     dir+'test\\images\\AUTOMOTIVE OIL\\3.jpg',
-    ],conf=0.8)  # predict on an image
+    ],conf=0.5)  # predict on an image
 
 # Process results list
 for i, result in enumerate(results):
@@ -25,7 +25,7 @@ for i, result in enumerate(results):
     result.show()  # display to screen
     
     # Construa o caminho completo para o arquivo
-    save_path = os.path.join(dir+'results', f'result_v4_{i}.jpg')
+    save_path = os.path.join(dir+'results', f'result_v5_{i}.jpg')
     
     # Salve a imagem no caminho especificado
     result.save(filename=save_path)
