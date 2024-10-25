8b9a759c17f9bfe6c7e15626f356b2bca47e2cd5
Pedrocanoas <pedro28canoas@gmail.com>
2024-10-24 21:30:17 -0300

    ajustes finais no modelo da ia

--- main.py
+++ main.py
@@ -2,28 +2,36 @@ import os
 import ultralytics
 from ultralytics import YOLO
 
-ultralytics.checks()
 dir = 'datasets\\'
 
-# model = YOLO('yolov8n.yaml')  # build a new model from scratch
-model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
+def main():
+    ultralytics.checks()  # Realiza verificações básicas para garantir que o ambiente está configurado corretamente
+    model = YOLO('yolov8m.pt')  # Carrega um modelo pré-treinado (recomendado para treinamento)
 
-results = model.train(data="config.yaml", epochs=200, patience=100, val=True) # train the model
-results = model.val(conf=0.5,save_json=True) # evaluate model performance on the validation set
-results = model([
-    dir+'test\\store.jpg'
-    ],conf=0.5)  # predict on an image
+    # Treine o modelo usando o conjunto de dados especificado no arquivo config.yaml
+    # epochs=200: Treina o modelo por 200 épocas
+    # patience=100: Define a paciência do treinamento para 100 épocas (quantidade de épocas sem melhoria antes de parar)
+    # val=True: Realiza validação ao final de cada época de treinamento
+    # batch=-1: Ajusta automaticamente o tamanho do lote com base nos recursos disponíveis
+    # single_cls=True: Trata todas as classes como uma única classe durante o treinamento (útil para detecção de uma única classe)
+    results = model.train(data="config.yaml", epochs=200, patience=100, val=True, batch=1, single_cls=True)  
 
-# Process results list
-for i, result in enumerate(results):
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    # masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    result.show()  # display to screen
-    
-    # Construa o caminho completo para o arquivo
-    save_path = os.path.join(dir+'results', f'result_v6_{i}.jpg')
-    
-    # Salve a imagem no caminho especificado
-    result.save(filename=save_path)
+    # Avalia o desempenho do modelo no conjunto de validação
+    # conf=0.50: Define um limiar de confiança de 50% para as predições durante a validação
+    # save_json=True: Salva os resultados da avaliação em um arquivo JSON
+    results = model.val(conf=0.01, save_json=True)  
+
+    # Realiza a predição em uma imagem específica localizada no caminho 'datasets\\test\\store.jpg'
+    # conf=0.50: Define um limiar de confiança de 50% para as predições
+    results = model.predict(['datasets\\test\\store.jpg'], conf=0.4, show_labels=False)
+
+    # Process results list
+    for result in results:
+    # Exibe o resultado na tela, mostrando apenas as caixas delimitadoras e a confiabilidade, sem as labels
+        result.show(labels=False, conf=True)
+        # Constrói o caminho completo para salvar o arquivo com o resultado
+        save_path = os.path.join(dir + 'results', 'result_v3.jpg')
+        result.save(filename=save_path)
+
+if __name__ == '__main__':
+    main()
